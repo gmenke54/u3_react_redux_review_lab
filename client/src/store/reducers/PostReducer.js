e2272730e2a0db338c089260e37b14efcd0264a4
gmenke54 <menke.gw@gmail.com>
2022-01-06 16:55:39 -0800

    removed unneccesary comments

--- client/src/store/reducers/PostReducer.js
+++ client/src/store/reducers/PostReducer.js
@@ -30,37 +30,3 @@ const PostReducer = (state = iState, action) => {
 };
 
 export default PostReducer;
-
-// working file:
-// const { POST_LOADING_TYPE, GET_POSTS, UPDATE_POST } = require('../types');
-
-// const iState = {
-//   posts: [],
-//   postsLoading: '' // Should be type enum('Loading', 'Loaded', 'Inactive')
-// };
-
-// const PostReducer = (state = iState, action) => {
-//   switch (action.type) {
-//     case POST_LOADING_TYPE:
-//       return { ...state, postsLoading: action.payload };
-//     case GET_POSTS:
-//       return { ...state, posts: action.payload };
-//     case UPDATE_POST:
-//       let i = state.posts.findIndex((post) => {
-//         if (post.id === action.payload._id) {
-//           return true;
-//         }
-//       });
-//       state.posts[i] = action.payload;
-//       // let updatedPosts = state.posts
-//       // let targetPost = updatedPosts.find((post)=> {
-//       //   return post._id === action.payload.id
-//       // })
-//       // targetPost.likes++
-//       return { ...state };
-//     default:
-//       return { ...state };
-//   }
-// };
-
-// export default PostReducer;
